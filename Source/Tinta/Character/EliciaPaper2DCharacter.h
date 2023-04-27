3b5f169ae10ef0766970a4e55308b8a42fade239
Miguel Yepes <miguelyegarcia94@gmail.com>
2023-04-26 21:44:02 -0600

    Added sprites and created FLipbooks for Elicia.

---
+++ Source/Tinta/Character/EliciaPaper2DCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "EliciaPaper2DCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINTA_API AEliciaPaper2DCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
