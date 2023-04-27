35d213e1804bfb4301f58ba3ca29484cd9b9dab3
Miguel Yepes <miguelyegarcia94@gmail.com>
2023-04-26 20:15:42 -0600

    Created empty project

---
+++ Source/Tinta/TintaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TintaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TINTA_API ATintaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
